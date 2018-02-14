353608906693d17a915cff4ccc665fc83171e122
muthu-dell-machine <muthuraj.bharathi@sstsinc.com>
2018-02-14 17:58:25 +0530

    removed bokkeddevices each

--- tradie/pCloudySample/sampleTest.js
+++ tradie/pCloudySample/sampleTest.js
@@ -1,7 +1,7 @@
 var Promise = require('promise'),
 logger = require('./helpers/logger.js'),
 pcloudyConnector = require('./api/pCloudyApiConnector.js');
-var pCloudyConfig = require('./wdio.conf.pcloudy.js');
+//var pCloudyConfig = require('./wdio.conf.pcloudy.js');
 var utils = require('./helpers/utils.js'),
 utilServices = new utils(),
 readline = require('readline'),
@@ -24,12 +24,12 @@ module.exports = function appiumPcloudy() {
       console.log('configs new ==== ');
       var pointer = this;
       var promise = new Promise(function(resolve,reject){
-      console.log("configPath "+configPath);	
+      console.log("configPath "+configPath);
       //utilServices.fileRead(configPath).then(function(configs) {
           try {
               //configs = JSON.parse(configs.data);
               //configs = pCloudyConfig.config;
-                 
+
               console.log('config json ==' + configs.host);
               var cloudName = configs.host,
               email = configs.username,
@@ -49,7 +49,7 @@ module.exports = function appiumPcloudy() {
               } else {
                   logger.info(' token  ====== > ' + response.result.token);
                   token = response.result.token; //saved in global variable
-            
+
                       var platformName = configs.platform,
                       devicePlatform = '-NOT-SELECTED-';
 
@@ -109,7 +109,7 @@ resolve(appiumLaunchStatus);
                                                                   logger.error("Service Launch error : "+appiumLaunchErr);
                                                                   var releaseStat = JSON.parse(appiumLaunchErr);
                                                                   logger.error('Error Status of Appium session release '+releaseStat.result.msg);
-                                                              
+
 reject(appiumLaunchErr);
 })
                                                               //core
@@ -174,7 +174,7 @@ return promise;
                       availabledevs = [],
                       sessionname = '';
                       if (allDevsavilable.length) {
-                         
+
                       } else {
                           logger.warn(" == There no devices available at this time try to book after some time == ");
                           pointer.terminate();
@@ -199,7 +199,7 @@ return promise;
                               pcloudyConnectorServices.BookDevicesForAppium(devDetails.token, 1, chosenDevs, platform, 'pcloudytest-' + platform, "true").then(function(bookDevstatus) {
                                   console.log('book devices ' + JSON.stringify(bookDevstatus));
                                   var bookedDevDetails = JSON.parse(bookDevstatus);
-                                  
+
                                   bookedDevDetails = bookedDevDetails.result;
                                   if(bookedDevDetails.hasOwnProperty('error')){
                                       logger.error("Error while booking devices : "+bookedDevDetails.error);
@@ -234,94 +234,9 @@ return promise;
                                                                     //logger.info(JSON.stringify(endPoint));
                                                                     logger.info("\n ===================== Started Appium and Received Endpoint ================== \n ");
                                                                     logger.info(" endpoint  ==> " + endPoint.endpoint);
-resolve({'status':'done','endpoint':endPoint.endpoint,'token':initHubresp.token,'bookedDevices':bookedDevices})
+                                                                    resolve({'status':'done','endpoint':endPoint.endpoint,'token':initHubresp.token,'bookedDevices':bookedDevices})
                                                                     var options = {};
-                                                                    try{
-                                                                    var totalBokkedDevs = bookedDevices.length;
 
-                                                                        bookedDevices.forEach(function(i, index, bookedDevices) {
-                                                                            console.log("insideeee booked devicess");
-                                                                            console.log("desired capabilities == " + configs.desiredCapabilities);
-                                                                            options.desiredCapabilities = {};
-                                                                            options.desiredCapabilities.launchTimeout = configs.desiredCapabilities.launchTimeout;
-                                                                            options.desiredCapabilities.CommandTimeout = configs.desiredCapabilities.CommandTimeout;
-                                                                            options.desiredCapabilities.deviceName = i.capabilities.deviceName;
-                                                                            options.desiredCapabilities.browserName = i.capabilities.browserName;
-                                                                            options.desiredCapabilities.platformName = i.capabilities.platformName;
-                                                                            options.desiredCapabilities.appPackage = configs.desiredCapabilities.appPackage;
-                                                                            options.desiredCapabilities.appActivity = configs.desiredCapabilities.appActivity;
-                                                                            options.desiredCapabilities.rotatable = configs.desiredCapabilities.rotatable;
-                                                                            options.logLevel = configs.logLevel;
-                                                                            options.logOutput = configs.logOutput;
-                                                                            options.protocol = configs.protocol;
-                                                                            options.host = configs.host;
-                                                                            options.port = configs.port;
-                                                                            options.coloredLogs = configs.coloredLogs;
-                                                                            options.bail = configs.bail;
-                                                                            options.screenshotPath = configs.screenshotPath;
-                                                                            options.screenshotOnReject = configs.screenshotOnReject;
-
-                                                                            var hubUrl = endPoint.endpoint + '/wd/hub';
-                                                                            var p = options.protocol + "://" + options.host;
-                                                                            options.path = hubUrl.split(p)[1];
-                                                                            var unixTime = Math.round(+new Date() / 1000);
-                                                                            pointer.timeConverter(unixTime).then(function(readableTime) {
-                                                                                unixTime = readableTime;
-                                                                            })
-
-                                                                            //var client = webdriverio.remote(options)
-                                                                            //.init().saveScreenshot(configs.screenshotPath + '/pcloudy-' + i.manufacturer + '-' + i.model + '-' + i.version + '-' + i.capabilities.deviceName + '-' + unixTime + '.png');
-                                                                            logger.info("*################################################### Add your Appium Code Here  #####################################*");
-                                                                           
-
-                                                                            
-                                                                           model = i.model,rid = i.rid;
-                                                                           /* setTimeout(function(){
-                                                                                logger.info('Going to end webdriver client of '+model);
-                                                                                client.end();
-                                                                                pcloudyConnectorServices.releaseAppiumsession(token,rid,0).then(function(releaseAppiumsession){
-      									                                                              logger.info('\n\n Releasing the Appium Session of '+model);
-                                                                                      var releaseStat = JSON.parse(releaseAppiumsession);
-                                                                                      releaseStat = releaseStat.result;
-                                                                                      if(releaseStat.hasOwnProperty('error')){
-                                                                                          logger.error("\n\n There was a error while releasing appium session "+releaseStat.error);
-                                                                                      }else{
-                                                                                          logger.info('\n\n Status of Appium session release for model : '+model + ' ==  '  +releaseStat.msg);
-                                                                                      }
-                                                                                  },function(releaseAppiumsessionErr){
-                                                                                      logger.error('\n releaseAppiumsession '+JSON.stringify(releaseAppiumsessionErr));
-                                                                                  })
-
-                                                                            },60000)*/
-                                                                            /*################################################## Add your code ################################################*/
-                                                                            console.log("hi jas");
-                                                                            //const jhkasdfjhlhasdf = require("./wdio.android.conf.js").fork;
-
-                                                                            //const spawn = require('child_process').spawn;
-//const ls = spawn('./node_modules/.bin/wdio', ['wdio.android.conf.js', '--suite=sanity']);
-
-/*ls.stdout.on('data', (data) => {
-    console.log(`stdout: ${data}`);
-});
-
-ls.stderr.on('data', (data) => {
-    console.log(`stderr: ${data}`);
-});
-
-ls.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
-});*/
-
-
-                                                                            logger.info(" Webdriver Initiated for  : " + i.model);
-                                                                            if(bookedDevices[index + 1]){
-                                             var next = bookedDevices[index + 1].manufacturer+'--'+bookedDevices[index + 1].model;
-//logger.debug("Webdriver Init Next : " + ((bookedDevices.length - 1 === index) ? resolve({'status':'done','endpoint':endPoint.endpoint,'token':initHubresp.token,'options':options}) : next));
-                                                                            }
-                                                                        })
-                                                                    }catch(errrr){
-                                                                        logger.error("options set Each err : "+errrr);
-                                                                    }
                                                                 }
                                                             }, function(getAppiumEndPointErr) {
                                                                 logger.Error(" getAppiumEndPointErr : "+JSON.stringify(getAppiumEndPointErr));
@@ -369,7 +284,7 @@ ls.on('close', (code) => {
       releasePCloudy: function(){
           console.log('after the script ----------- RELEASE' + rid);
           logger.info('Going to end webdriver client of '+model);
-         
+
           pcloudyConnectorServices.releaseAppiumsession(token,rid,0).then(function(releaseAppiumsession){
               logger.info('\n\n Releasing the Appium Session of '+ model);
               var releaseStat = JSON.parse(releaseAppiumsession);
